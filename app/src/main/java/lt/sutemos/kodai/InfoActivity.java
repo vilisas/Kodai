beae036c97f482e5a9df7b700a646e4ac40447cc
Vilius Icicle <vilisas@gmail.com>
2018-12-29 20:18:43 +0200

    info activity sukurtas, pataisytas ir main activity, pavertus telefona vaizdas issidesto graziai

--- app/src/main/java/lt/sutemos/kodai/InfoActivity.java
+++ app/src/main/java/lt/sutemos/kodai/InfoActivity.java
@@ -3,7 +3,7 @@ package lt.sutemos.kodai;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Info extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
